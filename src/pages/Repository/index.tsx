f9c84dc8a888db52ab7412cde1faf987fe4bc9e7
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-09-03 00:22:50 -0300

    📕  class 10 - repository page styles

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
-import { useRouteMatch } from 'react-router-dom';
+import { Link, useRouteMatch } from 'react-router-dom';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+
+import logo from '../../assets/logo.svg';
+import * as S from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -8,7 +12,56 @@ interface RepositoryParams {
 
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
-  return <h1>Repository: {params.repository}</h1>;
+
+  return (
+    <>
+      <S.Header>
+        <img src={logo} alt="GIthub Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={20} />
+          Voltar
+        </Link>
+      </S.Header>
+
+      <S.RepositoryInfo>
+        <header>
+          <img src="https://github.com/maurodesouza.png" alt="" />
+          <div>
+            <strong>asdadasdasd</strong>
+            <span>adadada</span>
+          </div>
+        </header>
+
+        <ul>
+          <li>
+            <strong>1234</strong>
+            <span>Stars</span>
+          </li>
+
+          <li>
+            <strong>1234</strong>
+            <span>Forks</span>
+          </li>
+
+          <li>
+            <strong>1234</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </S.RepositoryInfo>
+
+      <S.Issues>
+        <Link to="/repository">
+          <div>
+            <strong>repository.full_name</strong>
+            <span>epository.description</span>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </S.Issues>
+    </>
+  );
 };
 
 export default Repository;
