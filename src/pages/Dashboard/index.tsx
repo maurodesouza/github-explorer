6a3caaf5d21dc26585df72a2545468cbed04a599
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-09-02 22:35:26 -0300

    📗  class 6 - connect API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,101 +1,65 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
 import logo from '../../assets/logo.svg';
 import * as S from './styles';
+import api from '../../services/api';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  const handleAddRepository = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([repository, ...repositories]);
+    setNewRepo('');
+  };
+
   return (
     <>
       <img src={logo} alt="Github Explorer" />
 
       <S.Title>Explore repositórios no GitHub</S.Title>
 
-      <S.Form>
-        <input placeholder="Digite o repositório aqui" />
+      <S.Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o repositório aqui"
+        />
         <button type="submit">Pesquisar</button>
       </S.Form>
 
       <S.Repositories>
-        <a href="teste">
-          <img src="https://github.com/maurodesouza.png" alt="Mauro de Souza" />
-          <div>
-            <strong>maurodesouza/vscode-simple-readme</strong>
-            <p>A simple readme generator for VS Code</p>
-          </div>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
 
-          <FiChevronRight size={20} />
-        </a>
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-        <a href="teste">
-          <img src="https://github.com/maurodesouza.png" alt="Mauro de Souza" />
-          <div>
-            <strong>maurodesouza/vscode-simple-readme</strong>
-            <p>A simple readme generator for VS Code</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img src="https://github.com/maurodesouza.png" alt="Mauro de Souza" />
-          <div>
-            <strong>maurodesouza/vscode-simple-readme</strong>
-            <p>A simple readme generator for VS Code</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img src="https://github.com/maurodesouza.png" alt="Mauro de Souza" />
-          <div>
-            <strong>maurodesouza/vscode-simple-readme</strong>
-            <p>A simple readme generator for VS Code</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img src="https://github.com/maurodesouza.png" alt="Mauro de Souza" />
-          <div>
-            <strong>maurodesouza/vscode-simple-readme</strong>
-            <p>A simple readme generator for VS Code</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img src="https://github.com/maurodesouza.png" alt="Mauro de Souza" />
-          <div>
-            <strong>maurodesouza/vscode-simple-readme</strong>
-            <p>A simple readme generator for VS Code</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img src="https://github.com/maurodesouza.png" alt="Mauro de Souza" />
-          <div>
-            <strong>maurodesouza/vscode-simple-readme</strong>
-            <p>A simple readme generator for VS Code</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img src="https://github.com/maurodesouza.png" alt="Mauro de Souza" />
-          <div>
-            <strong>maurodesouza/vscode-simple-readme</strong>
-            <p>A simple readme generator for VS Code</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </S.Repositories>
     </>
   );
